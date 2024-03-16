55715e5c80d54a3dca99e23eaecaefd3d6388e00
derilmnzes <derilmenezes02@gmail.com>
2024-03-16 13:42:04 +0400

    new changes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,9 @@
-import React, { useEffect, useState } from 'react';
+import  { useEffect, useState, ChangeEvent } from 'react';
 import axios from 'axios';
 import Autocomplete from '@mui/material/Autocomplete';
 import { Button, IconButton, TextField } from '@mui/material';
 import CloseIcon from '@mui/icons-material/Close';
 
-
-// Define TypeScript types for the fetched data
 interface CarData {
   vin: string;
   country: string;
@@ -14,15 +12,26 @@ interface CarData {
   years: number[];
 }
 
+interface CarDetails {
+  city_mpg: number;
+  highway_mpg: number;
+  combination_mpg: number;
+  cylinders: number;
+  drive: string;
+  fuel_type: string;
+  make: string;
+  model: string;
+  transmission: string;
+  year: number;
+}
+
 function App() {
-  const [searchData, setSearchData] = useState('');
-  const [carData, setCarData] = useState<CarData | null>(null); // State to store the fetched car data
-  const [options, setOptions] = useState<string[]>([]); // State to store the Autocomplete options
+  const [searchData, setSearchData] = useState<string>('');
+  const [carData, setCarData] = useState<CarData | null>(null);
+  const [options, setOptions] = useState<string[]>([]);
   const [selectedModal, setSelectedModal] = useState<string>('');
-  const [carModal, setCarModal] = useState([]);
-  const [cars, setCars] = useState([]);
-  const [carDetails, setCarDetails] = useState()
-  const [toggleState,setToggleState] = useState(false)
+  const [cars, setCars] = useState<CarDetails[]>([]);
+  const [toggleState, setToggleState] = useState<boolean>(false);
 
   const fetchData = async () => {
     try {
@@ -37,22 +46,18 @@ function App() {
     }
   };
 
-
-
-
-
   const fetchModal = async () => {
     try {
       if (carData) {
-        const response = await axios.get<CarData>(`https://api.api-ninjas.com/v1/cars?limit=100&make=${carData.manufacturer}`, {
+        const response = await axios.get<CarData[]>(`https://api.api-ninjas.com/v1/cars?limit=100&make=${carData.manufacturer}`, {
           headers: {
             'X-Api-Key': '2bljrtZK68k9jItHBQpMqg==w39JtJtYjtQeZOiX',
           }
         });
         const updateArray = response.data.map((i) => {
+          //@ts-ignore
           return i.model;
         });
-
         setOptions(updateArray);
       }
     } catch (error) {
@@ -60,123 +65,112 @@ function App() {
     }
   };
 
-
   const handleSlectedModal = async () => {
     try {
-      const response = await axios.get<CarData>(`https://api.api-ninjas.com/v1/cars?limit=100&make=${carData?.manufacturer}&model=${selectedModal}`, {
+      const response = await axios.get<CarDetails[]>(`https://api.api-ninjas.com/v1/cars?limit=100&make=${carData?.manufacturer}&model=${selectedModal}`, {
         headers: {
           'X-Api-Key': '2bljrtZK68k9jItHBQpMqg==w39JtJtYjtQeZOiX',
         }
       });
-setToggleState(true)
-
-      setCars(response.data)
+      setToggleState(true);
+      setCars(response.data);
     } catch (err) {
-
+      console.error('Failed to fetch car details:', err);
     }
-  }
-
+  };
 
   useEffect(() => {
-    handleSlectedModal()
-  }, [selectedModal])
+    if (selectedModal) {
+      handleSlectedModal();
+    }
+  }, [selectedModal]);
 
-
-
-  // Function to handle Autocomplete input change
-  const handleAutoCompleteChange = async (event: React.ChangeEvent<{}>, value: string | null) => {
+  const handleAutoCompleteChange = (event: ChangeEvent<{}>, value: string | null) => {
+    console.log(event)
     if (value) {
       setSelectedModal(value);
     }
   };
 
-  // Function to handle input change
-  const handleInputChange = async (event: React.ChangeEvent<HTMLInputElement>) => {
+  const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
     setSearchData(event.target.value);
   };
 
   useEffect(() => {
-    fetchModal()
-  }, [carData])
+    fetchModal();
+  }, [carData]);
 
   return (
     <div className='flex items-center justify-center flex-col h-[100vh] w-[100vw]'>
-      {toggleState ? <div className='bg-white shadow-lg h-full w-[100%] m-auto rounded-lg p-10'>
-     <div className='flex items-end justify-end my-2'>
-     <IconButton onClick={()=>setToggleState(false)}>
-       <CloseIcon />
-       </IconButton>
-     </div>
-      <div className='overflow-x-auto'>
-  <table className='min-w-full divide-y divide-gray-200'>
-    <thead className='bg-gray-50'>
-      <tr>
-        {["City Milage", "Highway MPG", "Combination Milage", "Cylenders", "Drive", "Fuel Type", "Manufacturer", "Modal", "Transmission", "Year"].map((item, index) => (
-          <th key={index} scope="col" className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
-            {item}
-          </th>
-        ))}
-      </tr>
-    </thead>
-    <tbody className='bg-white divide-y divide-gray-200'>
-      {cars.map((item, index) => (
-        <tr key={index}>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.city_mpg}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.highway_mpg}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.combination_mpg}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.cylinders}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.drive}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.fuel_type}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.make}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.model}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.transmission}</td>
-          <td className='px-6 py-4 whitespace-nowrap'>{item.year}</td>
-        </tr>
-      ))}
-    </tbody>
-  </table>
-</div>
-
-   
-
-      </div> : <div className='bg-white shadow-lg rounded-lg p-10'>
-        {/* New input field */}
-        <input placeholder='Vin Number' className='px-5 py-2 border ' style={{
-          marginBottom: 20
-        }} value={searchData} onChange={handleInputChange} />
-        <Button onClick={fetchData}>Search</Button>
-        {/* Material-UI Autocomplete component */}
-        <TextField
-          fullWidth
-          value={carData?.manufacturer || ''}
-          style={{
-            marginBottom: 10
-          }}
-          label="Manufacturer"
-          variant="outlined"
-        />
-        <Autocomplete
-          options={options}
-          freeSolo
-          value={selectedModal}
-          onSelect={(e) => {
-            setSelectedModal(e.target.value)
-          }}
-          fullWidth
-          onChange={handleAutoCompleteChange}
-          renderInput={(params) => (
-            <TextField
-              fullWidth
-              {...params}
-              label="Model"
-              variant="outlined"
-            />
-          )}
-        />
-
-
-
-      </div>}
+      {toggleState ? (
+        <div className='bg-white shadow-lg h-full w-[100%] m-auto rounded-lg p-10'>
+          <div className='flex items-end justify-end my-2'>
+            <IconButton onClick={() => setToggleState(false)}>
+              <CloseIcon />
+            </IconButton>
+          </div>
+          <div className='overflow-x-auto'>
+            <table className='min-w-full divide-y divide-gray-200'>
+              <thead className='bg-gray-50'>
+                <tr>
+                  {["City Milage", "Highway MPG", "Combination Milage", "Cylinders", "Drive", "Fuel Type", "Manufacturer", "Modal", "Transmission", "Year"].map((item, index) => (
+                    <th key={index} scope="col" className='px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider'>
+                      {item}
+                    </th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody className='bg-white divide-y divide-gray-200'>
+                {cars.map((item, index) => (
+                  <tr key={index}>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.city_mpg}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.highway_mpg}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.combination_mpg}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.cylinders}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.drive}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.fuel_type}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.make}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.model}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.transmission}</td>
+                    <td className='px-6 py-4 whitespace-nowrap'>{item.year}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      ) : (
+        <div className='bg-white shadow-lg rounded-lg p-10'>
+          <input placeholder='Vin Number' className='px-5 py-2 border ' style={{ marginBottom: 20 }} value={searchData} onChange={handleInputChange} />
+          <Button onClick={fetchData}>Search</Button>
+          <TextField
+            fullWidth
+            value={carData?.manufacturer || ''}
+            style={{ marginBottom: 10 }}
+            label="Manufacturer"
+            variant="outlined"
+          />
+          <Autocomplete
+            options={options}
+            freeSolo
+            value={selectedModal}
+            onSelect={(e) => {
+              //@ts-ignore
+              setSelectedModal(e.target.value);
+            }}
+            fullWidth
+            onChange={handleAutoCompleteChange}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                fullWidth
+                label="Model"
+                variant="outlined"
+              />
+            )}
+          />
+        </div>
+      )}
     </div>
   );
 }
